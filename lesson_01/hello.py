0547a0b095064425e46f76ab7fbd21213766a024
Jim Johnson <email_programmer_jim@yahoo.com>
2026-02-20 20:52:38 -0600

    Old files were deleted in prep for new lecture series file structure.

--- lesson_01/hello.py
+++
@@ -1,7 +0,0 @@
-greeting = "Hello, World!"
-print(greeting)
-
-# To run this code, save it 
-# Click chevron (downward arrow) next to white hollow arrow in the top right corner 
-# file editor pane, select "Run 'hello'" from the dropdown menu,
-# or simply press Shift + F10 on your keyboard.
